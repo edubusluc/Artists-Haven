6f04f41250282b5ff2e1b58699b647e38b015f22
edubusluc <edubusluc@alum.us.es>
2025-01-09 17:21:28 +0100

    fix: refactor UserController and UserService to use constructor injection for dependencies

--- artists-heaven-backend/src/main/java/com/artists_heaven/entities/user/UserService.java
+++ artists-heaven-backend/src/main/java/com/artists_heaven/entities/user/UserService.java
@@ -14,8 +14,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
