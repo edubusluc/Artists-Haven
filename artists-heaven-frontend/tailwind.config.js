5a2dd0ca6ce3f21143e863816d02dcd2cd82c21a
edubusluc <edubusluc@alum.us.es>
2025-06-09 10:11:40 +0200

    feat: add new font files and update styles for custom fonts in the frontend

---
+++ artists-heaven-frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      dropShadow: {
+        'strong': '0 6px 8px rgba(0, 0, 0, 0.4)',
+      },
+    },
+  },
+  plugins: [],
+}
